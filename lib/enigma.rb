39d019b7ae53fee697d56b7e7346b502080f5bf1
Ashkan Abbasi <airashkan@yahoo.com>
2020-04-18 17:25:22 -0600

    Changed Enigma to implament module.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,2 @@
-require 'date'
-
 class Enigma
-  attr_reader :alphabet, :date
-
-  def initialize
-    @alphabet = ("a".."z").to_a << " "
-    @date = Date.today.strftime("%d%m%y")
-  end
-
-end
+end 
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,4 @@
 require_relative 'test_helper'
-require "./lib/enigma"
-require 'date'
 
 class EnigmaTest < Minitest::Test
 
@@ -11,14 +9,4 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
-
-  def test_it_has_attributes
-    charset = ("a".."z").to_a << " "
-
-    assert_equal charset, @enigma.alphabet
-    Date.stubs(:today).returns('05091979')
-    assert_equal '05091979', @enigma.date
-  end
-
-
 end
