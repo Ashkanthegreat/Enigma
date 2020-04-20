6b114d6636c79352a031218614e0480994e5ad83
Ashkan Abbasi <airashkan@yahoo.com>
2020-04-19 19:45:03 -0600

    Set Offset attributes and test that Offset can get todays date.

--- lib/offset.rb
+++ lib/offset.rb
@@ -2,6 +2,10 @@ require_relative 'randomizer'
 
 class Offset
   include Randomizer
-  
+  attr_reader :date
+
+  def initialize(date = todays_date)
+    @date = date
+  end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -3,10 +3,23 @@ require_relative 'test_helper'
 class OffsetTest < Minitest::Test
 
   def setup
-    @offset = Offset.new
+    @offset = Offset.new('05091979')
   end
 
   def test_key_exists
     assert_instance_of Offset, @offset
+  end
+
+  def test_offset_attrributes
+    assert_equal '05091979', @offset.date
+  end
+
+  def test_offset_can_get_todays_date
+    Date.stubs(:today).returns('05091979')
+    assert_equal '05091979', @offset.date
+  end
+
+  def test_it_can_create_an_offset
+    
   end
 end
