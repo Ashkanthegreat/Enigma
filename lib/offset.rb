2bdcbaed95ff048c6f8bb229adb0f4b58f36982c
Ashkan Abbasi <airashkan@yahoo.com>
2020-04-19 22:41:36 -0600

    Create Generate Offset method, helper methods, and tests.

--- lib/offset.rb
+++ lib/offset.rb
@@ -8,4 +8,21 @@ class Offset
     @date = date
   end
 
+  def date_squared
+    (@date.to_i**2).to_s
+  end
+
+  def offset_values
+    date_squared[-4..-1]
+  end
+
+  def generate_offset
+    offset = Hash.new
+    offset[:A] = offset_values[0]
+    offset[:B] = offset_values[1]
+    offset[:C] = offset_values[2]
+    offset[:D] = offset_values[3]
+    offset
+  end
+
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -3,7 +3,7 @@ require_relative 'test_helper'
 class OffsetTest < Minitest::Test
 
   def setup
-    @offset = Offset.new('05091979')
+    @offset = Offset.new('050979')
   end
 
   def test_key_exists
@@ -11,15 +11,24 @@ class OffsetTest < Minitest::Test
   end
 
   def test_offset_attrributes
-    assert_equal '05091979', @offset.date
+    assert_equal '050979', @offset.date
   end
 
   def test_offset_can_get_todays_date
-    Date.stubs(:today).returns('05091979')
-    assert_equal '05091979', @offset.date
+    Date.stubs(:today).returns(Date.new(2020,4,19))
+    assert_equal "190420", @offset.todays_date
+  end
+
+  def test_can_square_date
+    assert_equal "2598858441", @offset.date_squared
+  end
+
+  def test_offset_values
+    assert_equal "8441", @offset.offset_values
   end
 
   def test_it_can_create_an_offset
-    
+    expected = {:A=>"8", :B=>"4", :C=>"4", :D=>"1"}
+    assert_equal expected, @offset.generate_offset
   end
 end
