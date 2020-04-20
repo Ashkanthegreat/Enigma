e234a5be9c8007616398f3d0cc2f77f104afea2e
Ashkan Abbasi <airashkan@yahoo.com>
2020-04-20 11:54:45 -0600

    Create Shift class and test it exists.

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,3 +1,14 @@
+require_relative 'randomizer'
+require_relative 'key'
+require_relative 'offset'
+
 class Shift
+  include Randomizer
+
+
+  def initialize(key, offset)
+    @key = Key.new(random_keys)
+    @offset = Offset.new(todays_date)
+  end
 
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -2,4 +2,11 @@ require_relative 'test_helper'
 
 class ShiftTest < Minitest::Test
 
+  def setup
+    @shift = Shift.new(@key, @offset)
+  end
+
+  def test_key_exists
+    assert_instance_of Shift, @shift
+  end
 end
