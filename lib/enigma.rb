2565eeb17505aab6e0d9e4d7f2d3ffc0e59768be
Ashkan Abbasi <airashkan@yahoo.com>
2020-04-21 02:27:37 -0600

    Took pry out of Enigma.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,7 +8,6 @@ class Enigma
   def encrypt(message, key = random_keys, date = todays_date )
     shifts = Shift.new(key.to_i, date)
     encrypt_shift = shifts.combine_shifts_with_direction(1)
-    require "pry"; binding.pry
     {
       encryption: encrypt_shift.transform_message(message, shifts),
       key: key,
